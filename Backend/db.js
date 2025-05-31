4d3fe882cb2ebb5f8439370aa192a84e379749c9
KrishSharma007 <136921215+KrishSharma007@users.noreply.github.com>
2025-05-31 19:46:59 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToMongo = async () => {
   try {
-    const mongoURI =
-      process.env.MONGODB_URI ||
-      "mongodb://localhost:27017/inventory_management";
+    const mongoURI ="mongodb+srv://krishsharma8105:Krish@007@cluster0.hidhi6n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
     await mongoose.connect(mongoURI);
     console.log("Connected to MongoDB successfully");
   } catch (error) {
